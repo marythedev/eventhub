743fd02ca50c9d6d4713820ebfb717e0acd5e37f
Maria <79389256+marythedev@users.noreply.github.com>
2023-08-18 21:44:14 -0400

    added models

--- eventhub/apps/events/admin.py
+++ eventhub/apps/events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Seat, Concert, Payment, Ticket, Order
+
+admin.site.register(User)
+admin.site.register(Concert)
+admin.site.register(Seat)
+admin.site.register(Payment)
+admin.site.register(Ticket)
+admin.site.register(Order)

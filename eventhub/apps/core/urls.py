9e061bc707cb49e4d648da527c67234de133408c
marythedev <marythedev@gmail.com>
2025-12-09 17:19:25 -0500

    deprecate contact feature due to smtp production unreliability

--- eventhub/apps/core/urls.py
+++ eventhub/apps/core/urls.py
@@ -6,33 +6,33 @@ from .forms import PasswordResetValidator
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('contact/', views.contact, name="contact"),
+    # path('contact/', views.contact, name="contact"),
     path('terms-and-conditions/', views.terms_and_conditions, name="terms_and_conditions"),
 
-    path('password-reset/',
-        auth_views.PasswordResetView.as_view(
-            subject_template_name='core/password-reset-email/password_reset_subject.txt',
-            email_template_name='core/password-reset-email/password_reset_email.txt',
-            template_name='core/password_reset.html'
-        ),
-        name='password_reset'),
+    # path('password-reset/',
+    #     auth_views.PasswordResetView.as_view(
+    #         subject_template_name='core/password-reset-email/password_reset_subject.txt',
+    #         email_template_name='core/password-reset-email/password_reset_email.txt',
+    #         template_name='core/password_reset.html'
+    #     ),
+    #     name='password_reset'),
 
-    path('password-reset/done/',
-        auth_views.PasswordResetDoneView.as_view(
-            template_name='core/password_reset_done.html'
-        ),
-        name='password_reset_done'),
+    # path('password-reset/done/',
+    #     auth_views.PasswordResetDoneView.as_view(
+    #         template_name='core/password_reset_done.html'
+    #     ),
+    #     name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-            form_class=PasswordResetValidator,
-            template_name='core/password_reset_confirm.html'
-        ),
-        name='password_reset_confirm'),
+    # path('reset/<uidb64>/<token>/',
+    #     auth_views.PasswordResetConfirmView.as_view(
+    #         form_class=PasswordResetValidator,
+    #         template_name='core/password_reset_confirm.html'
+    #     ),
+    #     name='password_reset_confirm'),
 
-    path('reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name='core/password_reset_complete.html'
-        ),
-        name='password_reset_complete'),
+    # path('reset/done/',
+    #     auth_views.PasswordResetCompleteView.as_view(
+    #         template_name='core/password_reset_complete.html'
+    #     ),
+    #     name='password_reset_complete'),
 ]
